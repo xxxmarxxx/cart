ada83159ec67ca269ab503b542fe45463b9a9a53
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-05 15:37:03 +0100

    lesson 163

--- src/context.js
+++ src/context.js
@@ -28,22 +28,23 @@ const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: "DECREASE", payload: id });
   };
-  const fetchData =async()=>{
-    dispatch({type:'LOADING'})
-    const response = await fetch(url)
-    const cart = await response.json()
-    dispatch({type:'DISPLAY_ITEMS', payload:cart})
-  }
-  useEffect(()=>{
-    fetchData()
-  },[])
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+  const toggleAmount = (id, type) => {
+    dispatch({ type: "TOGGLE_AMOUNT", payload: { id, type } });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   useEffect(() => {
     // console.log('hello')
     dispatch({ type: "GET_TOTALS" });
   }, [state.cart]);
-
-
 
   return (
     <AppContext.Provider
@@ -53,6 +54,7 @@ const AppProvider = ({ children }) => {
         remove,
         increase,
         decrease,
+        toggleAmount,
       }}
     >
       {children}
